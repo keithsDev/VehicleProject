089bb1993abfcf34f7655782a8ce1be04bf1df52
Keith Kenneally <keithkenneally08@gmail.com>
2018-01-26 00:58:39 +0100

    inital files

---
+++ Vehicle/src/Bike.java
@@ -0,0 +1,13 @@
+
+public class Bike {
+
+	private String colour;
+	
+	
+	String getColour(){
+		return this.colour;
+	}
+	void setColour(String c){
+		this.colour = c;
+	}
+}
---
+++ Vehicle/src/VehicleTest.java
@@ -0,0 +1,15 @@
+
+public class VehicleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bike b = new Bike();
+		b.setColour("Green");
+		
+		System.out.println("the colour of bike b is "+ b.getColour());
+		
+
+	}
+
+}
